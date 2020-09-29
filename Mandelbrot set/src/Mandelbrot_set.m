5602a81600b9a4f8f6aaa0400218a7e6a3886978
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2020-09-30 00:11:34 +0200

    clean up the repo

---
+++ Mandelbrot set/src/Mandelbrot_set.m
@@ -0,0 +1,24 @@
+step = 0.01;
+Imax = 500;
+Re_plot = -2.1:step:0.7;
+Im_plot = -1.4:step:1.4;
+
+for Re = Re_plot
+    for Im = Im_plot
+        z = 0;
+        counter = 0;
+        while abs(z) < 2 && counter < Imax
+            z = z^2 + Re + Im*1i;
+            counter = counter + 1;
+        end
+        if counter == Imax
+            inSet = true;
+        else
+            inSet = false;
+        end
+        if inSet
+            plot(Re, Im, 'k.');
+            hold on
+        end
+    end
+end
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# MatLAB
+# MATLAB
+
+**MATLAB** is a high-performance language developed by MathWorks for technical computing, visualization, and programming. 
+
+## Projects
+
+- Mandelbrot set
+- Weather station
+
+## Todo
+
+- [ ] add comments to Mandelbrot set code.
